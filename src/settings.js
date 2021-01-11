527c91d490ea77cf1e0e069b7ecedc2926543939
pakping <pakping-up@hotmail.com>
2021-01-11 16:09:34 +0700

    first commit

---
+++ src/settings.js
@@ -0,0 +1,11 @@
+export default{
+    firebaseConfig : {
+        apiKey: "AIzaSyAcBZ9TSRNkOQPe9hazigag2uL-lp0NzIo",
+        authDomain: "kafare-up-bafe8.firebaseapp.com",
+        projectId: "kafare-up-bafe8",
+        storageBucket: "kafare-up-bafe8.appspot.com",
+        messagingSenderId: "106475792057",
+        appId: "1:106475792057:web:d3332afd20000b22149d68",
+        measurementId: "G-0Q8JF1QRQ7"
+      }
+}
